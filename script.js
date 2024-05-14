139541ffcaff0cb7d2e7197744354fa36e88f9e3
Andhika Ihsan C <andhikaihsan79@gmail.com>
2024-05-14 11:49:58 +0700

    update

--- script.js
+++ script.js
@@ -1,67 +1,80 @@
+// Constants
+const LINE_WIDTH = 5;
+const LINE_CAP = 'round';
+
+// Get the canvas element and its context
 const canvas = document.getElementById('signatureCanvas');
 const ctx = canvas.getContext('2d');
+
+// Get the clear button element
+const clearButton = document.getElementById('clearButton');
+
+// Get the save button element
+const saveButton = document.getElementById('saveButton');
+
+// Initialize variables
 let drawing = false;
 let lastX, lastY;
 let signatureBox;
 
-// Mouse events
-canvas.addEventListener('mousedown', (e) => {
+// Function to handle drawing start
+function startDrawing(event) {
   drawing = true;
-  lastX = e.clientX - canvas.offsetLeft;
-  lastY = e.clientY - canvas.offsetTop;
+  lastX = getCoordinate(event, 'clientX') - canvas.offsetLeft;
+  lastY = getCoordinate(event, 'clientY') - canvas.offsetTop;
   ctx.beginPath();
   ctx.moveTo(lastX, lastY);
-});
+}
 
-canvas.addEventListener('mousemove', (e) => {
+// Function to handle drawing move
+function draw(event) {
   if (drawing) {
-    ctx.lineWidth = 5;
-    ctx.lineCap = 'round';
-    ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    ctx.lineWidth = LINE_WIDTH;
+    ctx.lineCap = LINE_CAP;
+    lastX = getCoordinate(event, 'clientX') - canvas.offsetLeft;
+    lastY = getCoordinate(event, 'clientY') - canvas.offsetTop;
+    ctx.lineTo(lastX, lastY);
     ctx.stroke();
     ctx.beginPath();
-    ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    ctx.moveTo(lastX, lastY);
   }
-});
+}
 
-canvas.addEventListener('mouseup', () => {
+// Function to save the canvas as an image
+function saveCanvas() {
   drawing = false;
-});
+  // Create a new image element
+  const image = new Image();
+  // Set the source of the image to the canvas
+  image.src = canvas.toDataURL();
+  // Replace the canvas with the image
+  canvas.parentNode.replaceChild(image, canvas);
+}
 
-// Touch events
-canvas.addEventListener('touchstart', (e) => {
-  drawing = true;
-  lastX = e.touches[0].clientX - canvas.offsetLeft;
-  lastY = e.touches[0].clientY - canvas.offsetTop;
-  ctx.beginPath();
-  ctx.moveTo(lastX, lastY);
-});
-
-canvas.addEventListener('touchmove', (e) => {
-  if (drawing) {
-    ctx.lineWidth = 5;
-    ctx.lineCap = 'round';
-    ctx.lineTo(e.touches[0].clientX - canvas.offsetLeft, e.touches[0].clientY - canvas.offsetTop);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(e.touches[0].clientX - canvas.offsetLeft, e.touches[0].clientY - canvas.offsetTop);
-  }
-});
-
-canvas.addEventListener('touchend', () => {
+// Function to handle drawing end
+function endDrawing() {
   drawing = false;
-});
+}
 
-// Clear button
-document.getElementById('clear').addEventListener('click', () => {
+// Function to clear the canvas
+function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-});
+}
 
-// Save button
-document.getElementById('save').addEventListener('click', () => {
-  drawing = false;
-  signatureBox = new Image();
-  signatureBox.src = canvas.toDataURL();
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(signatureBox, 0, 0, 200, 100);
-});
+// Function to get coordinate from event
+function getCoordinate(event, coordinate) {
+  return event.touches? event.touches[0][coordinate] : event[coordinate];
+}
+
+// Add event listeners
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', endDrawing);
+
+canvas.addEventListener('touchstart', startDrawing);
+canvas.addEventListener('touchmove', draw);
+canvas.addEventListener('touchend', endDrawing);
+
+clearButton.addEventListener('click', clearCanvas);
+
+saveButton.addEventListener('click', saveCanvas);
